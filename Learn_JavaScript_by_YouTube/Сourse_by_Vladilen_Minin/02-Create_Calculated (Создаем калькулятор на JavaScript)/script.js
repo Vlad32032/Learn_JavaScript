c079435ee42a6bba0dbc1f1bba1516717204dee6
Vlad Sazonov <vlad32032@mail.ru>
2023-09-07 04:40:43 +0300

    Finished make calculated

---
+++ Learn_JavaScript_by_YouTube/Сourse_by_Vladilen_Minin/02-Create_Calculated (Создаем калькулятор на JavaScript)/script.js
@@ -0,0 +1,43 @@
+const resultElement = document.getElementById("result");
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const submitBtn = document.getElementById("submit");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+let action = "+";
+
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value);
+    const num2 = Number(inp2.value);
+
+    if (actionSymbol == "+") {
+        return num1 + num2;
+    } else if (actionSymbol == "-") {
+        return num1 - num2;
+    };
+};
+
+function printResult(result) {
+    if (result < 0) {
+        resultElement.style.color = "red";
+    } else {
+        resultElement.style.color = "green";
+    };
+
+    resultElement.textContent = result;
+};
+
+
+plusBtn.onclick = function () {
+    action = "+";
+};
+
+minusBtn.onclick = function () {
+    action = "-";
+};
+
+submitBtn.onclick = function () {
+    const result = computeNumbersWithAction(input1, input2, action);
+    printResult(result);
+};
