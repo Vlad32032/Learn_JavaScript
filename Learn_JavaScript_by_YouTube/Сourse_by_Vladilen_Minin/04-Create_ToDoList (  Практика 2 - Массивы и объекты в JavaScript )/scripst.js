723ac5aa86700bc44c83e574f701e8cb3cc53654
Vlad Sazonov <vlad32032@mail.ru>
2023-09-09 04:48:38 +0300

    Add more function

--- Learn_JavaScript_by_YouTube/Сourse_by_Vladilen_Minin/04-Create_ToDoList (  Практика 2 - Массивы и объекты в JavaScript )/scripst.js
+++ Learn_JavaScript_by_YouTube/Сourse_by_Vladilen_Minin/04-Create_ToDoList (  Практика 2 - Массивы и объекты в JavaScript )/scripst.js
@@ -2,6 +2,33 @@ const inputElement = document.getElementById("title");
 const createBtn = document.getElementById("create");
 const listElement = document.getElementById("list");
 
+const notes = ["Запись 1", "Запись 2", "Запись 3"];
+
+function getNoteTemplate(title) {
+    return `
+        <li
+            class="list-group-item d-flex
+            justify-content-between align-items-center"
+        >
+            <span>${title}</span>
+            <span>
+                <button class="btn btn-small btn-success">&check;</button>
+                <button class="btn btn-small btn-danger">&times;</button>
+            </span>
+        </li>
+    `;
+};
+
+function render(arrayNotes) {
+    // for (let i = 0; i < arrayNotes.length; i++) {
+    //     listElement.insertAdjacentHTML("beforeend", getNoteTemplate(arrayNotes[i]));
+    // };
+
+    for (let note of arrayNotes) {
+        listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
+    };
+};
+
 createBtn.onclick = function () {
     if (inputElement.value.length === 0) {
         return
@@ -37,3 +64,5 @@ createBtn.onclick = function () {
 
     inputElement.value = "";
 };
+
+render(notes);
