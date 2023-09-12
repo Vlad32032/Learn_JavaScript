1cb256af94a8adb50580ed6db644759b5008391f
Vlad Sazonov <vlad32032@mail.ru>
2023-09-12 21:16:26 +0300

    Add more theory

--- Learn_JavaScript_by_YouTube/Сourse_by_Vladilen_Minin/06-Base_JavaScript ( Методы массивов )/script.js
+++ Learn_JavaScript_by_YouTube/Сourse_by_Vladilen_Minin/06-Base_JavaScript ( Методы массивов )/script.js
@@ -1,3 +1,45 @@
-const names = ["Влад", "Наташа", "Полина", "Марк"];
+/* Методы массивов */
 
-names.push("Алена");
+const namesOne = ["Влад", "Наташа", "Полина", "Марк"];
+const namesTwo = ["Катя","Лена","Света","Лена"];
+
+const numsOne = [3, 54, 5, 12, 2, 1, 7, 17];
+const numsTwo = [3, 54, 5, 12, 2, 1, 7, 17];
+
+namesTwo.push("Алена"); // Добавить в конец массива (быстрая операция)
+console.log("namesTwo -", namesTwo);
+
+namesTwo.unshift("Кристина"); // Добавить в начало массива (долгая операция)
+console.log("namesTwo -", namesTwo);
+
+namesTwo.shift(); // Метод shift удаляет(извлекает) первый элемент массива и возвращает этот элемент
+// например:
+let nameFromStart = namesTwo.shift();
+console.log("nameFromStart -", nameFromStart);
+console.log("namesTwo -", namesTwo);
+
+namesTwo.pop(); // Метод pop удаляет(извлекает) последний элемент массива и возвращает этот элемент
+// например:
+let nameFromEnd = namesTwo.pop();
+console.log("nameFromEnd -", nameFromEnd);
+console.log("namesTwo -", namesTwo);
+
+namesTwo.reverse(); // Метод reverse переворачивает массив(было - [1, 2, 3, 4], станет - [4, 3, 2, 1])
+console.log("namesTwo -", namesTwo);
+
+console.log("numsOne -", numsOne);
+console.log("numsTwo -", numsTwo);
+numsTwo.sort();
+console.log("numsTwo -", numsTwo);
+
+// -- Все эти методы мутируют(изменяют) исходный массив --
+
+const reversedNames = namesTwo.toReversed(); // Метод toReversed делает то же, что reverse но не мутирует массив а вовзвращает новый
+console.log("namesTwo -", namesTwo);
+console.log("reversedNames -", reversedNames);
+
+
+
+
+
+
