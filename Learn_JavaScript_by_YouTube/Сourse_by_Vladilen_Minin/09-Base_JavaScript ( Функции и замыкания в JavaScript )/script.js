de6ff720a14423a904bf8ce347cdfc889eca458a
Vlad Sazonov <vlad32032@mail.ru>
2023-09-20 07:34:47 +0300

    Start theory about fanction

--- Learn_JavaScript_by_YouTube/Сourse_by_Vladilen_Minin/09-Base_JavaScript ( Функции и замыкания в JavaScript )/script.js
+++ Learn_JavaScript_by_YouTube/Сourse_by_Vladilen_Minin/09-Base_JavaScript ( Функции и замыкания в JavaScript )/script.js
@@ -1,4 +1,25 @@
-/* Строки в деталях */
+/* Функции и замыкания */
 
 // -- Откройте консоль --
 
+
+
+// Function Declaration
+
+function greetOne(name) {
+    console.log('Hello -', name)
+}
+// можно вызвать до объявления функции
+
+// Function Expression
+
+const greetTwo = function (name) {
+    console.log('Hello -', name)
+}
+// нельзя вызвать до объявления функции
+
+console.log(typeof greetOne) // function
+// typeof покажет function но это для удобства разработки
+// на самом деле function это объект
+// можем в этом убедиться
+console.dir(greetOne)
